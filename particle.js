49647fba47bffe6e6763632096b0c7acc0b87b8d
Ranveer200810 <79564951+Ranveer200810@users.noreply.github.com>
2021-05-20 13:48:28 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,36 @@
+class Particle {
+
+    constructor(x,y,r) {
+
+        var opt = {
+
+            restitution: 0.4,
+            friction: 0.3
+
+        }
+
+        this.body = Bodies.circle(x, y, r, opt);
+        this.r = r;
+        this.color = color(random(0,255), random(0,255), random(0,255));
+        World.add(world, this.body);
+
+    }
+
+    display() {
+
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+
+        translate(pos.x, pos.y);
+        rotate(angle);
+        fill(this.color);
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.r, this.r);
+
+        pop();
+
+    }
+
+}
